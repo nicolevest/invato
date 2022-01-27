00417903a229da56c31076173308d98297385ad5
nicolevest <nicovillarreale@gmail.com>
2022-01-27 15:46:24 -0500

    first commit

---
+++ resources/views/componentes/barra_nav.blade.php
@@ -0,0 +1,18 @@
+<nav>
+    <ul>
+        <li>
+            <a href="">Informes</a>
+        </li>
+
+        <li>
+            <a href="">Activos</a>
+        </li>
+        <li>
+            <a href="">Operaciones</a>
+        </li>
+
+        <li>
+            <a href="config/{{$username}}">Configuración</a>
+        </li>
+    </ul>
+</nav>
