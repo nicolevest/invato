34a2b03259ddf175c48345ee31a56825d0c53907
Maria Jose Guerrero <majog9714@gmail.com>
2022-04-08 00:37:57 -0500

    finalizados estilos de tablas y home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,20 +27,20 @@
 
             <!-- Three columns of text below the carousel -->
             <div class="row mt-4">
-                <div class="col-lg-4">
-                    <img class="rounded-circle" src="https://i7.uihere.com/icons/429/380/1020/goods-6cac497f8866e87e2b8e997417347f14.png" alt="Generic placeholder image" width="140" height="140">
+                <div class="carta col-lg-4">
+                    <img class="rounded-circle" src="/img/recepcion.png" alt="Generic placeholder image" width="140" height="140">
                     <h2>Recepcion</h2>
                     <p>Pendientes: {{ $recibidos }}</p>
                     <p><a class="btn btn-secondary botonvermas" href="/recepcion_act" role="button">Ver más</a></p>
                 </div><!-- /.col-lg-4 -->
-                <div class="col-lg-4">
-                    <img class="rounded-circle" src="https://iconape.com/wp-content/files/xh/367685/svg/send-logo-icon-png-svg.png" alt="Generic placeholder image" width="140" height="140">
+                <div class="carta col-lg-4">
+                    <img class="rounded-circle" src="/img/traslado.png" alt="Generic placeholder image" width="140" height="140">
                     <h2>Traslados</h2>
                     <p>Pendientes: {{ $en_transito }}</p>
                     <p><a class="btn btn-secondary botonvermas" href="/traslado_act" role="button">Ver más</a></p>
                 </div><!-- /.col-lg-4 -->
-                <div class="col-lg-4">
-                    <img class="rounded-circle" src="https://icon-library.com/images/x-icon-png/x-icon-png-25.jpg" alt="Generic placeholder image" width="140" height="140">
+                <div class="carta col-lg-4">
+                    <img class="rounded-circle" src="/img/baja.png" alt="Generic placeholder image" width="140" height="140">
                     <h2>Dados de baja</h2>
                     <p>Pendientes: {{ $dados_de_baja }}</p>
                     <p><a class="btn btn-secondary botonvermas" href="/salida_act" role="button">Ver más</a></p>
